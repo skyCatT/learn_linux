e84167c82a50d0eea924beabdb0845ec7e5ecb7b
cat <zhxh@localhost.localdomain>
2019-03-27 19:34:30 +0800

    myhello

---
+++ myhello/p3/build/CMakeFiles/libhello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libhello.dir/hello.cpp.o"
+  "liblibhello.pdb"
+  "liblibhello.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libhello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ myhello/p4/build/libhello/CMakeFiles/libhello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libhello.dir/hello.cpp.o"
+  "../../lib/liblibhello.pdb"
+  "../../lib/liblibhello.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libhello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
